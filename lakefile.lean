934a666038d4e1e0c0b8c8168e3d73ebae88040a
Daniel Weber <commandmaster2017@gmail.com>
2024-04-12 20:28:55 +0300

    Fix hopefully

--- lakefile.lean
+++ lakefile.lean
@@ -11,10 +11,6 @@ require mathlib from git
 require LeanAPAP from git
   "https://github.com/YaelDillies/LeanAPAP.git"
 
-
-require PFR from git
-  "https://github.com/teorth/pfr.git"
-
 @[default_target]
 lean_lib «Pseudorandom» {
   -- add any library configuration options here
